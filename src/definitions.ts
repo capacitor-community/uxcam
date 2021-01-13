31b0d8cf6ce360a60db16cc941bcc4249bd3a264
Luis Alaguna <alagunasalahaddin@live.com>
2021-01-13 17:03:18 +0300

    ☀️ adding definitions and web implementation.

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,18 +1,36 @@
-declare module "@capacitor/core" {
+declare module '@capacitor/core' {
   interface PluginRegistry {
-    CapacitorAppAnalytics: CapacitorAppAnalyticsPlugin;
+    UXCam: UXCamPlugin;
   }
 }
 
 export interface UserProperty {
-  userId: string
-  name: string
-  userInfo: {[key: string]: string}
+  properties: { [key: string]: string | number };
 }
 
-export interface CapacitorAppAnalyticsPlugin {
+export interface LogEventProperty {
+  eventName: string;
+  properties?: { [key: string]: string | number };
+}
+
+export interface UXCamPlugin {
+  echo(options: { value: string }): Promise<{ value: string }>;
+
+  logEvent(options: LogEventProperty): Promise<void>;
+
   startWithKey(options: { UXCamKey: string }): Promise<void>;
+
+  setUserIdentity(options: { userIdentity: string }): Promise<void>;
+
   setUserProperty(options: UserProperty): Promise<void>;
-  tagScreenName(options: { screenName: string }): Promise<void>
+
+  setMultiSessionRecord(options: { recordMultipleSessions: boolean }): Promise<void>;
+
+  getEnabledMultiSessionRecord(): Promise<{ value: boolean }>;
+
+  tagScreenName(options: { screenName: string }): Promise<void>;
+
+  setAutomaticScreenNameTagging(options: { enable: boolean }): Promise<void>;
+
   stopSession(): Promise<void>
 }
