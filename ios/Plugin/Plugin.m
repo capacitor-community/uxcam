a4c6a2c1f7e9eb769622d271ecba43440bbecd9b
Luis Alaguna <alagunasalahaddin@live.com>
2021-01-13 17:18:22 +0300

    ☀️ adding support for iOs.

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -3,9 +3,15 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(CapacitorAppAnalytics, "CapacitorAppAnalytics",
+CAP_PLUGIN(UXCam, "UXCam",
+           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(logEvent, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(startWithKey, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setUserIdentity, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(setUserProperty, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setMultiSessionRecord, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getEnabledMultiSessionRecord(), CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(tagScreenName, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(stopSeaaion, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(setAutomaticScreenNameTagging, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stopSession, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -2,43 +2,80 @@ import Foundation
 import Capacitor
 import UXCam
 
-@objc(CapacitorAppAnalytics)
-public class CapacitorAppAnalytics: CAPPlugin {
-    
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitorjs.com/docs/plugins/ios
+ */
+@objc(UXCam)
+public class UXCam: CAPPlugin {
+
+    @objc func echo(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.success([
+            "value": value
+        ])
+    }
+
     @objc func startWithKey(_ call: CAPPluginCall) {
-        let UXCamKey = call.getString("UXCamKey") ?? ""
+        String eventName = call.getString("eventName") ?? ""
+        let properties = call.getObject("properties", defaultValue: [:])
+
+        if (properties.isEmpty) UXCam.logEvent(eventName)
+        else UXCam.logEvent(eventName,withProperties: properties)
+    }
+
+    @objc func startWithKey(_ call: CAPPluginCall) {
+        String UXCamKey = call.getString("UXCamKey") ?? ""
         let type = "cordova"
-        
+
         UXCam.pluginType(type, version: "3.1.5")
         UXCam.optIntoSchematicRecordings()
         UXCam.start(withKey: UXCamKey, buildIdentifier: nil)
-        
+
         call.success()
     }
-    
+
+    @objc func setUserIdentity(_ call: CAPPluginCall) {
+        String userIdentity = call.getString("userIdentity") ?? ""
+        UXCam.setUserIdentity(userIdentity)
+        call.success()
+    }
+
     @objc func setUserProperty(_ call: CAPPluginCall) {
-        let userId = call.getString("userId") ?? ""
-        let name = call.getString("name") ?? ""
-        
-        UXCam.setUserIdentity(name)
-        UXCam.setUserProperty("user_id", value: userId)
-        UXCam.setUserProperty("alias", value: name)
-        
-        let userInfo = call.getObject("userInfo", defaultValue: [:])
-        
+        let userInfo = call.getObject("properties", defaultValue: [:])
+
         for (key, value) in userInfo ?? [:] {
             UXCam.setUserProperty(key, value: value)
         }
         call.success()
     }
-    
+
+    @objc func setMultiSessionRecord(_ call: CAPPluginCall) {
+        Bool enabled = call.getString("recordMultipleSessions") ?? ""
+        UXCam.setMultiSessionRecord(enabled)
+        call.success()
+    }
+
+    @objc func getEnabledMultiSessionRecord(_ call: CAPPluginCall) {
+        Bool enabled = UXCam.getMultiSessionRecord()
+        call.success([
+            "value": enabled
+        ])
+    }
+
     @objc func tagScreenName(_ call: CAPPluginCall) {
-        let screenName = call.getString("screenName") ?? ""
-        
+        String screenName = call.getString("screenName") ?? ""
+
         UXCam.tagScreenName(screenName)
         call.success()
     }
-    
+
+    @objc func setAutomaticScreenNameTagging(_ call: CAPPluginCall) {
+        Bool enable = call.getString("enable") ?? ""
+        UXCam.setAutomaticScreenNameTagging(enable)
+        call.success()
+    }
+
     @objc func stopSession(_ call: CAPPluginCall) {
         UXCam.stopSessionAndUploadData()
         call.success()
