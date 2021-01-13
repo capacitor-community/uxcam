0131dce873f0d56d3263da53800171a1a2a62e79
Luis Alaguna <alagunasalahaddin@live.com>
2021-01-13 17:18:34 +0300

    ☀️ adding simple android implementation.

---
+++ android/src/main/java/com/getcapacitor/community/uxcam/UXCam.java
@@ -0,0 +1,106 @@
+package com.getcapacitor.community.uxcam;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+
+@NativePlugin
+public class UXCam extends Plugin {
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.success(ret);
+    }
+
+    @PluginMethod()
+    public void logEvent(PluginCall call) {
+        String eventName = call.getString("eventName")
+        // JSObject rawProperties = call.getObject("properties", new JSObject());
+        //Iterator<String> userProperties = rawProperties.keys();
+        JSObject properties = call.getObject("properties", new JSObject());
+
+        if (properties.isEmpty()) UXCam.logEvent(eventName)
+        else UXCam.logEvent(eventName, properties)
+        /*{
+            final Map<String, String> properties = new HashMap<String, String>();
+            while (userProperties.hasNext()) {
+                String property = (String) userProperties.next();
+                String propertyValue = userInfo.getString(property);
+
+                properties.push(property, propertyValue)
+            }
+             UXCam.logEvent(eventName, properties)
+        }*/
+    }
+
+    @PluginMethod()
+    public void startWithKey(PluginCall call) {
+        String UXCamKey = call.getString("UXCamKey");
+        UXCam.startApplicationWithKeyForCordova(getActivity(), UXCamKey);
+        call.success();
+    }
+
+    @PluginMethod()
+    public void setUserIdentity(PluginCall call){
+        String userIdentity = call.getString("userIdentity");
+        UXCam.setUserIdentity(name);
+        call.success();
+    }
+
+    @PluginMethod()
+    public void setUserProperty(PluginCall call) {
+
+        JSObject userInfo = call.getObject("properties", new JSObject());
+        Iterator<String> userProperties = userInfo.keys();
+
+        while (userProperties.hasNext()) {
+            String property = (String) userProperties.next();
+            String propertyValue = userInfo.getString(property);
+
+            UXCam.setUserProperty(property, propertyValue);
+        }
+
+        call.success();
+    }
+
+    @PluginMethod()
+    public void setMultiSessionRecord(PluginCall call){
+        boolean enabled = call.getString("recordMultipleSessions")
+        UXCam.setMultiSessionRecord(enabled)
+        call.success()
+    }
+
+    @PluginMethod()
+    public boolean getEnabledMultiSessionRecord(PluginCall call){
+        JSObject ret = new JSObject();
+        boolean enabled = UXCam.getMultiSessionRecord();
+        ret.put("value", enabled);
+        call.success(ret);
+    }
+
+    @PluginMethod()
+    public void tagScreenName(PluginCall call) {
+        String screenName = call.getString("screenName");
+        UXCam.tagScreenName(screenName);
+        call.success();
+    }
+
+    @PluginMethod()
+    public void setAutomaticScreenNameTagging(PluginCall call) {
+        boolean enable = call.getString("enable")
+        UXCam.setAutomaticScreenNameTagging(enable)
+        call.success()
+    }
+
+    @PluginMethod()
+    public void stopSession(PluginCall call) {
+        UXCam.stopSessionAndUploadData();
+        call.success();
+    }
+}
